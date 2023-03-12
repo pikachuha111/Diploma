9e5e535bbab54b03374e72e41c89e0d98b6636b7
Nikita_Solovev <nickdanse99@gmail.com>
2023-03-13 01:31:34 +0300

    refactoring

--- src/test/java/tests/api/BookTests.java
+++ src/test/java/tests/api/BookTests.java
@@ -8,22 +8,22 @@ import org.testng.annotations.Test;
 public class BookTests extends BaseApiTest {
     protected CollectionBooks collectionBooks = new CollectionBooks();
 
-    @Test(description = "GET collection of books")
+    @Test(description = "GET (NFE) collection of books")
     public void getCollectionBooksTest() {
         collectionBooks = bookAdapter.getCollectionBooks();
     }
 
-    @Test(description = "GET book by name", dependsOnMethods = "getCollectionBooksTest")
+    @Test(description = "GET (NFE) book by name", dependsOnMethods = "getCollectionBooksTest")
     public void getBookByNameTest() {
         bookAdapter.getBookByName(collectionBooks, "git Pocket Guide");
     }
 
-    @Test(description = "POST add book to user who has been authorized", dependsOnMethods = "getCollectionBooksTest")
+    @Test(description = "POST (NFE) add book to user who has been authorized", dependsOnMethods = "getCollectionBooksTest")
     public void addBookToUserTest() {
         bookAdapter.addBookToUser(actualUser, collectionBooks, "git Pocket Guide");
     }
 
-    @Test(description = "GET user method", dependsOnMethods = "addBookToUserTest")
+    @Test(description = "GET (NFE) user method", dependsOnMethods = "addBookToUserTest")
     public void getUserTest() {
         userAdapter.getUser(actualUser);
     }
--- src/test/java/tests/api/UserTests.java
+++ src/test/java/tests/api/UserTests.java
@@ -6,17 +6,17 @@ import org.testng.annotations.Test;
 
 public class UserTests extends BaseApiTest {
 
-    @Test (description = "POST failed Log In test")
+    @Test (description = "POST (AFE) failed Log In test")
     public void failedLogInTest() {
         userAdapter.failedLogIn(incorrectUser);
     }
 
-    @Test (description = "POST user is not found test")
+    @Test (description = "POST (AFE) user is not found test")
     public void authorizedUserIsNotFoundTest() {
         userAdapter.failedAuthorizedUser(incorrectUser);
     }
 
-    @Test (description = "POST authorization by secure data")
+    @Test (description = "POST (NFE) authorization by secure data")
     public void adminAuthorization() {
         User admin = new User();
         admin.setUserName(System.getProperty("adminLogin"));
